31d4e8661a6bc7712815b199b502fdd1b133e00c
Adrianna Diaz <adrianna.dev@gmail.com>
2017-12-28 19:03:28 -0500

    Testing the server for Travis

---
+++ express-server/test/server.js
@@ -0,0 +1,9 @@
+// This is a dummy test file
+var assert = require('assert');
+
+describe('someMadeUpFunction(null)',function(){
+	it('There should be a description of the test', function(){
+		var someVar = true;
+		assert.equal(someVar, true);
+	});
+});
