677f9cf57452850b80a27d7634f2dcf15ab87832
Adrianna Diaz <adrianna.dev@gmail.com>
2017-12-30 22:18:27 -0500

    Changed the fields in the form

--- angular-client/src/app/components/new-participant/new-participant.component.ts
+++ angular-client/src/app/components/new-participant/new-participant.component.ts
@@ -12,29 +12,13 @@ export class NewParticipantComponent implements OnInit {
   public name: String;
   public address: String;
   public telephone: String;
-  public city: String;
-  public provinces = [
-    {value: "bc", viewValue: "British Columbia"},
-    {value: "qc", viewValue: "Québec"},
-    {value: "on", viewValue: "Ontario"},
-    {value: "ns", viewValue: "Nova Scotia"},
-    {value: "nb", viewValue: "New Brunswick"},
-    {value: "mb", viewValue: "Manitoba"},
-    {value: "pe", viewValue: "Prince Edward Island"},
-    {value: "sk", viewValue: "Saskatchewan"},
-    {value: "ab", viewValue: "Alberta"},
-    {value: "nl", viewValue: "NewFoundland and Labrador"},
-    {value: "nt", viewValue: "Northwest Territories"},
-    {value: "yt", viewValue: "Yukon"},
-    {value: "nu", viewValue: "Nunavut"}
-
-  ]
-  public postalcode: String;
+  public email: String;
+  public socialmedia: String;
 
   ngOnInit() {
   }
 
-  submit(){
-    //Send to profile service
+  submit() {
+    // Send to profile service
   }
 }
