4251ccb741bc943ba267925810722910f59460db
ldhatter <liuaihatter@gmail.com>
2017-12-30 19:25:50 -0500

    Added unstyled form

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -1,16 +1,43 @@
+import { platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialsModule } from './modules/materials.module';
 
 
 import { AppComponent } from './app.component';
+import { CreateProfileComponent } from './components/create-profile/create-profile.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'create-profile',
+    component: CreateProfileComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateProfileComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MaterialsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
