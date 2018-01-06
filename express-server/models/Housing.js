0873e50f79486d08277cd2ca52275139c448032c
Anna Rogozin <anna@rogozin.ca>
2018-01-06 17:27:54 -0500

    Added schemas for Resource and Housing resource Ref #3 Used mongoose discriminators to implement inheritance

---
+++ express-server/models/Housing.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Resource = require('../models/Resource').schema;
+
+const housingSchema = new Schema({
+    term: { type: String, required: true},
+    gender: String,
+    constraints: String
+  }, { discriminatorKey: 'kind', timestamps: true });
+
+const Housing = Resource.discriminator('Housing', housingSchema);
+
+module.exports = Housing;
