cc01e4aae7baa93a88409ddbba46bdc550b2e47a
Anna Rogozin <anna@rogozin.ca>
2018-01-08 14:49:21 -0500

    Added tests for Housing resources Ref #13

--- express-server/models/Housing.js
+++ express-server/models/Housing.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Resource = require('../models/Resource').schema;
+const Resource = require('../models/Resource');
 
 const housingSchema = new Schema({
     term: { type: String, required: true},
