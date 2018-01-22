077529edce7e1c3a46dc6cc834618f800c28908b
Adrianna Diaz <adrianna.dev@gmail.com>
2018-01-22 15:42:53 -0500

    #5 Added multiple shelter selection on the frontend

--- angular-client/src/app/components/case-modal/case-modal.component.ts
+++ angular-client/src/app/components/case-modal/case-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { CaseService } from '../../services/case.service';
+import { ResourceService } from '../../services/resource.service';
 import { Case } from '../../classes/case';
 
 
@@ -12,7 +13,8 @@ import { Case } from '../../classes/case';
 export class CaseModalComponent implements OnInit {
   statuses = ['In progress', 'Completed'];
   urgencies = ['Regular', 'Urgent'];
-  selectedStatus: String;
+  selectedUrgency: String;
+  resources: Object;
   case: Case = {
     _id: '',
     status: 'In progress',
@@ -22,15 +24,30 @@ export class CaseModalComponent implements OnInit {
     shelterchosen: '',
   };
 
+  /**
+   * Load all resources
+   * 
+   * @memberof ViewResourcesComponent
+   */
+  loadAllResources() {
+    this.resourceService.getAll()
+      .subscribe(data => {
+        console.log(data);
+        this.resources = data;
+      });
+  }
+
 
   constructor(
     private caseService: CaseService,
+    private resourceService: ResourceService,
     public dialogRef: MatDialogRef<CaseModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) {
   }
 
 
   ngOnInit() {
+    this.loadAllResources();
   }
 
   submit(): void {
