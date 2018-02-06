06639b5507ae34de957a7b44b7131f2b345e3879
Wahab Ahmed <wahabwajahat@hotmail.com>
2018-02-06 03:25:46 -0500

    Custom validations for add new participant.

--- angular-client/src/app/components/new-participant/new-participant.component.ts
+++ angular-client/src/app/components/new-participant/new-participant.component.ts
@@ -5,43 +5,42 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { AlertModalComponent } from '../alert-modal/alert-modal.component';
 import { AuthenticationService } from '../../services/authentication.service';
 import { RouterModule, Router } from '@angular/router';
-import { FormGroup, FormControl, Validators, FormBuilder } from "@angular/forms";
-
+import { FormGroup, FormControl, Validators,ValidatorFn, FormBuilder,ValidationErrors } from "@angular/forms";
 
 @Component({
   selector: 'app-new-participant',
   templateUrl: './new-participant.component.html',
   styleUrls: ['./new-participant.component.css']
 })
+
 export class NewParticipantComponent implements OnInit {
+  
 
   form: FormGroup;
-
-  // participantData: Participant = {
-  //   _id: '',
-  //   name: '',
-  //   address: '',
-  //   telephone: '',
-  //   email: '',
-  //   socialmedia: {service: '', username: '' }
-  // };
 
   isAlreadyAParticipantID = false;
   isAlreadyAParticipantEmail = false;
 
+  
   constructor(private fb :FormBuilder,private participantService: ParticipantService, public dialog: MatDialog, public authService: AuthenticationService, public router: Router) {
-   this.form = this.fb.group({
+
+
+    this.form = this.fb.group({
             name: ['', Validators.required ],
             _id: ['', Validators.required ],
            // password:new FormControl('',Validators.required),
             address:['', Validators.required ],
-            telephone:['', Validators.required ],
-            email:['', Validators.required ],
+            telephone:[''],
+            email:[''],
             socialmedia:this.fb.group({ // <-- the child FormGroup
-              username: ['', Validators.required ],
-              service: ['', Validators.required ],
+              username: [''],
+              service: [''],
      }),
-});
+}, {
+      validator:(formgroup:FormGroup)=>{
+        return this.validateInput(formgroup);
+        }
+      });
   }
 
   ngOnInit() {
@@ -50,6 +49,18 @@ export class NewParticipantComponent implements OnInit {
     }
     
      
+  }
+   validateInput(formgroup:FormGroup){
+     let socialun = formgroup.value.socialmedia.username;
+     let socials = formgroup.value.socialmedia.service;
+    if((formgroup.controls["telephone"].value || formgroup.controls["email"].value || socialun && socials)){
+  console.log('OK');
+  return null;
+  }
+  else {
+    console.log('Not OK');
+    return {validateInputData: true};
+  }
   }
 
   /**
@@ -97,23 +108,14 @@ export class NewParticipantComponent implements OnInit {
    * @memberof NewParticipantComponent
    */
   onSubmit() {
-        console.log("model-based form submitted");
-        console.log(this.form.value);
+    console.log(this.form.value);
+        this.participantService.save(this.form.value)
+      .subscribe(data => {
+        if (data.hasOwnProperty("errmsg")) {
+          this.alertModal("Could not add new participant.");
+        } else {
+          this.alertModal("New participant successfully added.")
+        }
+      })
 }
-
-  /**
-   * Submit new participant profile information.
-   * 
-   * @memberof NewParticipantComponent
-   */
-  // submit() {
-  //   this.participantService.save(this.participantData)
-  //     .subscribe(data => {
-  //       if (data.hasOwnProperty("errmsg")) {
-  //         this.alertModal("Could not add new participant.");
-  //       } else {
-  //         this.alertModal("New participant successfully added.")
-  //       }
-  //     })
-  // }
 }
