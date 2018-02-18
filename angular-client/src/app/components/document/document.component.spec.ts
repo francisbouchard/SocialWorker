9021d4cb9e4bc3cd451f891dd76008bed11ec656
Adrianna Diaz <adrianna.dev@gmail.com>
2018-02-18 11:12:47 -0500

    Made DocumentComponent Karma test pass

--- angular-client/src/app/components/document/document.component.spec.ts
+++ angular-client/src/app/components/document/document.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/authentication.service';
+import { MockAuthenticationService } from '../../mocks/MockAuthenticationService';
+import { ParticipantService } from '../../services/participant.service';
+import { MockParticipantService } from '../../mocks/MockParticipantService';
 import { DocumentComponent } from './document.component';
+import { MaterialsModule } from '../../modules/materials.module';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 describe('DocumentComponent', () => {
   let component: DocumentComponent;
@@ -8,7 +14,15 @@ describe('DocumentComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DocumentComponent ]
+      declarations: [ DocumentComponent ],
+      imports: [ MaterialsModule ],
+      providers: [
+        {provide: ParticipantService, useClass: MockParticipantService},
+        {provide: AuthenticationService, useClass: MockAuthenticationService},
+        { provide: Router, useValue: { navigateByUrl: jasmine.createSpy('navigateByUrl') } },
+        {provide: MatDialogRef, useValue: {} },
+        {provide: MAT_DIALOG_DATA, useValue: {} },
+      ]
     })
     .compileComponents();
   }));
