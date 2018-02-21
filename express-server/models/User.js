9300aa7bf577be4b3554396f6c1c287c8eea2956
Adrianna Diaz <adrianna.dev@gmail.com>
2018-02-20 21:45:37 -0500

    User not Users here too

--- express-server/models/User.js
+++ express-server/models/User.js
@@ -32,6 +32,6 @@ const userSchema = new mongoose.Schema({
     });
   };
 
-const User = mongoose.model('Users', userSchema, 'users');
+const User = mongoose.model('User', userSchema, 'users');
 
 module.exports = User;
