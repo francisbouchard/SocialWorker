547115f54fc33d3c1b8f9a04d85fb158824b930b
Wahab Ahmed <wahabwajahat@hotmail.com>
2018-02-22 23:53:39 -0500

    Started working on view phonelog component. #67

--- angular-client/src/app/components/phonelog/view-phonelog/view-phonelog.component.ts
+++ angular-client/src/app/components/phonelog/view-phonelog/view-phonelog.component.ts
@@ -12,16 +12,46 @@ import { PhonelogService } from '../../../services/phonelog.service';
 })
 export class ViewPhonelogComponent implements OnInit {
   @Input() hasTabChanged: boolean;
+    editingLog = Phonelog;
+    public logs;
+    public sortProperty = 'name';
+    public reverse = false;
+    public query: string;
+
 
   constructor(
     private phonelogService: PhonelogService,
     public authService: AuthenticationService,
     public router: Router) { }
 
+    loadLogs() {
+    if (this.authService.role === 'admin') {
+      this.phonelogService.getAll()
+        .subscribe(data => {
+          console.log(data)
+          this.logs = data;
+        });
+    }
+  }
+
+  /**
+   * Specify which participant is currently in edit mode
+   *
+   * @param {any} id
+   * @param {any} log
+   * @memberof ViewParticipantsComponent
+   */
+  edit(id, log) {
+    this.editingLog = log;
+  }
+
     ngOnInit() {
       if (!this.authService.loggedIn) {
         this.router.navigateByUrl('login');
       }
+      else {
+      this.loadLogs();
+    }
     }
 
 }
