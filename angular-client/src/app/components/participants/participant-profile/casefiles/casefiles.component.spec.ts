7f6ed1787bb0bc86da42c392be975498808e0c5f
ldhatter <liuaihatter@gmail.com>
2018-03-03 22:51:52 -0500

    #64 Tests: adding imports

--- angular-client/src/app/components/participants/participant-profile/casefiles/casefiles.component.spec.ts
+++ angular-client/src/app/components/participants/participant-profile/casefiles/casefiles.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CasefilesComponent } from './casefiles.component';
+import { MaterialsModule } from '../../../../modules/materials.module';
+import { FormControl, FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute, convertToParamMap, Router } from '@angular/router';
+import { AuthenticationService } from '../../../../services/authentication.service';
+import { MockAuthenticationService } from '../../../../mocks/MockAuthenticationService';
+
 
 describe('CasefilesComponent', () => {
   let component: CasefilesComponent;
@@ -8,7 +14,13 @@ describe('CasefilesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CasefilesComponent ]
+      imports: [ MaterialsModule, RouterTestingModule, FormControl, FormGroup, FormBuilder, FormArray, Validators],
+      declarations: [ CasefilesComponent ],
+      providers: [
+        { provide: AuthenticationService, useClass: MockAuthenticationService },
+        { provide: Router, useValue: { navigateByUrl: jasmine.createSpy('navigateByUrl') } },
+        { provide: ActivatedRoute, useValue: { snapshot: { paramMap: convertToParamMap({_id: 'frontendtest'}) } } }
+      ]
     })
     .compileComponents();
   }));
