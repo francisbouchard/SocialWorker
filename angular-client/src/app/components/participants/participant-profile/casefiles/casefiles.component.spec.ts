715acc935194e63d1c04edb37f8285e4b4d0617a
ldhatter <liuaihatter@gmail.com>
2018-03-03 22:58:35 -0500

    #64 Tests: adding forms and reactive forms

--- angular-client/src/app/components/participants/participant-profile/casefiles/casefiles.component.spec.ts
+++ angular-client/src/app/components/participants/participant-profile/casefiles/casefiles.component.spec.ts
@@ -2,11 +2,13 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CasefilesComponent } from './casefiles.component';
 import { MaterialsModule } from '../../../../modules/materials.module';
 import { FormControl, FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ActivatedRoute, convertToParamMap, Router } from '@angular/router';
 import { AuthenticationService } from '../../../../services/authentication.service';
 import { MockAuthenticationService } from '../../../../mocks/MockAuthenticationService';
-
+import { CasefileService } from '../../../../services/casefile.service';
+import { MockCasefileService } from '../../../../mocks/MockCasefileService';
 
 describe('CasefilesComponent', () => {
   let component: CasefilesComponent;
@@ -14,10 +16,11 @@ describe('CasefilesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ MaterialsModule, RouterTestingModule, FormControl, FormGroup, FormBuilder, FormArray, Validators],
+      imports: [ MaterialsModule, RouterTestingModule, FormsModule, ReactiveFormsModule],
       declarations: [ CasefilesComponent ],
       providers: [
         { provide: AuthenticationService, useClass: MockAuthenticationService },
+        { provide: CasefileService, useClass: MockCasefileService },
         { provide: Router, useValue: { navigateByUrl: jasmine.createSpy('navigateByUrl') } },
         { provide: ActivatedRoute, useValue: { snapshot: { paramMap: convertToParamMap({_id: 'frontendtest'}) } } }
       ]
