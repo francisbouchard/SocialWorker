a067f157065dde8866fb48d94663f66918c1c56e
Adrianna Diaz <adrianna.dev@gmail.com>
2018-03-14 17:20:22 -0400

    Changed server.js to include medical resource #62

--- express-server/server.js
+++ express-server/server.js
@@ -79,8 +79,8 @@ app.use('/api/casefile', casefile);
 app.use('/api/phonelog', phonelog);
 
 app.use('/api/resource', resource);
-app.use('/api/resource/housing', resource);
-app.use('/api/resource/medical', resource);
+app.use('/api/resource/housing', housing);
+app.use('/api/resource/medical', medical);
 
 
 // Server public folder
