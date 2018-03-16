007df474461bcd949ba1e54b338adc9f0d34d4da
Wahab Ahmed <wahabwajahat@hotmail.com>
2018-03-16 11:21:32 -0400

    Updated backend tests and made name required in the model. #85

--- express-server/test/participant.test.js
+++ express-server/test/participant.test.js
@@ -5,9 +5,9 @@ const mongoose = require('mongoose');
 const server = require('../server');
 const Participant = require('../models/Participant');
 
-let id1 = 'testingID1';
+let id1 = new mongoose.Types.ObjectId();
 let id2 = 'testingID2';
-let id3 = 'testingID3';
+let id3 = new mongoose.Types.ObjectId();
 let noteId = new mongoose.Types.ObjectId();
 let docId = new mongoose.Types.ObjectId();
 let workerId1 = new mongoose.Types.ObjectId("5a7f87c0e146e233d707518b");
@@ -30,6 +30,7 @@ describe('Participant Tests', () => {
             });
         let participant1 = new Participant({
             _id: id1,
+            username:"test",
             name: 'participant1',
             pronouns: 'she/her',
             email: 'participant1@p.com',
@@ -48,6 +49,7 @@ describe('Participant Tests', () => {
         });
         let participant3 = new Participant({
             _id: id3,
+            username:"test2",
             name: 'participant3',
             email: 'participant3@p.com',
             telephone: '514-1234567'
@@ -81,7 +83,7 @@ describe('Participant Tests', () => {
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('object');
-                    res.body.should.have.property('_id');
+                    res.body.should.have.property('username');
                     res.body.should.have.property('name');
                     res.body.should.have.property('pronouns');
                     res.body.should.have.property('email');
@@ -95,7 +97,7 @@ describe('Participant Tests', () => {
         });
         it('should be empty for GET with nonexisting ID', (done) => {
             chai.request(server)
-                .get('/api/participant/id/' + 'p123')
+                .get('/api/participant/id/')
                 .set('Cookie', cookie)
                 .end((err, res) => {
                     res.should.have.status(200);
@@ -155,9 +157,8 @@ describe('Participant Tests', () => {
     });
 
     describe('/POST', () => {
-        it('should not POST a participant without _id', (done) => {
+        it('should not POST a participant without name', (done) => {
             let participant = {
-                name: 'participant',
                 email: 'participant@p.com',
                 telephone: '514-1234567'
             }
@@ -169,14 +170,13 @@ describe('Participant Tests', () => {
                     res.should.have.status(200);
                     res.body.should.be.a('object');
                     res.body.should.have.property('errors');
-                    res.body.errors.should.have.property('_id');
-                    res.body.errors._id.should.have.property('kind').eql('required');
+                    res.body.errors.should.have.property('name');
+                    res.body.errors.name.should.have.property('kind').eql('required');
                     done();
                 });
         });
         it('should POST a participant', (done) => {
             let participant = {
-                _id: id2,
                 name: 'participant',
                 pronouns: 'they/them',
                 email: 'participant2@p.com',
@@ -189,7 +189,7 @@ describe('Participant Tests', () => {
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('object');
-                    res.body.should.have.property('_id');
+                    res.body.should.have.property('username');
                     res.body.should.have.property('name');
                     res.body.should.have.property('pronouns');
                     res.body.should.have.property('email');
