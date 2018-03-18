2b5099c4f121347c5ca8b85a92469413695ab6c6
Adrianna Diaz <adrianna.dev@gmail.com>
2018-03-18 18:37:54 -0400

    Changing medical field schemas #62

--- angular-client/src/app/classes/medical.ts
+++ angular-client/src/app/classes/medical.ts
@@ -1,7 +1,7 @@
 import { Resource } from './resource';
 
 export class Medical extends Resource {
-    without_cost: Boolean;
+    without_cost: { type: String};
     waitlist_time: { type: String};
-    schedule_availability: [String];
+    schedule_availability:  { type: String};
 }
--- express-server/models/resources/Medical.js
+++ express-server/models/resources/Medical.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema;
 const Resource = require('../../models/Resource');
 
 const medicalSchema = new Schema({
-    without_cost: Boolean,
+    without_cost: { type: String},
     waitlist_time: { type: String},
-    schedule_availability: [String]
+    schedule_availability: { type: String},
   }, { discriminatorKey: 'kind', timestamps: true });
 
 const Medical = Resource.discriminator('Medical', medicalSchema);
